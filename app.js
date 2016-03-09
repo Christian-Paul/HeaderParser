757a561f225d5fab0f2a8a4f60c8b987fdd72e05
Christian Paul <christiansamuelpaul@gmail.com>
2016-03-08 23:59:14 -0500

    enabled trust proxy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
+app.enable('trust proxy');
 
 app.get('/', function(req, res) {
     var ip, language, software;
