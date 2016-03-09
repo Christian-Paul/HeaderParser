f62b864c25fd8a7d5afa09aa742850557d0fec37
Christian Paul <christiansamuelpaul@gmail.com>
2016-03-08 23:36:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    var ip, language, software;
+    
+    ip = req.ip;
+    language = req.get('Accept-Language').split(',')[0];
+    software = req.get('User-Agent').split('(')[1].split(')')[0];
+    
+    res.json({ IP: ip, Language: language, Software: software });
+});
+
+app.listen(port);
